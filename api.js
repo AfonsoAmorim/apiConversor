8bfe8a86ea453b7c95e4b2afca37c426f7f48096
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-10 21:53:16 -0300

    acessando cotação atual

--- api.js
+++ api.js
@@ -2,5 +2,11 @@ let url = "https://economia.awesomeapi.com.br/last/USD-BRL"
 
 fetch(url)
 .then((res)=>{
-    console.log(res);
+    return res.json();
+    
+
 })
+.then((data)=>{
+    console.log(data.USDBRL.high);
+})
+
